1a9b03569efee62b4a1dace3c00ceb0b8f72a01f
Walter Holmgren <walter@holmgren.fi>
2023-11-26 07:17:54 +0200

    latest windows stuff

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -6,6 +6,21 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
+if vim.fn.has("win32") then
+  local powershell_options = {
+    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
+end
+
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
